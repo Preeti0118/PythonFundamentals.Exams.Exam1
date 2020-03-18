6f747a2a5aa03aa04e9d076440c99dfc065fe27f
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-18 14:15:22 -0400

    done

--- exam1/loopty_loop.py
+++ exam1/loopty_loop.py
@@ -10,7 +10,10 @@ def generate_list(start: int, stop: int, step: int = 1) -> List[int]:
     :param step: How many digits apart each number is from the others around it.
     :return: A list of integers.
     """
-    pass  # implement me
+
+    list_out = []
+    list_out = list(range(start, stop, step))
+    return list_out
 
 
 def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Callable) -> List[int]:
@@ -23,4 +26,8 @@ def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Call
     :param strategy: A function to manipulate each digit .
     :return: A list of integers.
     """
-    pass  # implement me
+
+    list_out = []
+    for i in range(start, stop, step):
+        list_out.append(strategy(i))
+    return list_out
